67fdb1b486207de9552f99edc59766e4a61e42a3
FlipFlop <lamandealexis@gmail.com>
2022-12-05 13:48:14 +0100

    fzeferdbufd

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from './pages/Home'
+import { BrowserRouter , Route, Routes } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+				</Routes>
+		</BrowserRouter>
+
   </React.StrictMode>,
   document.getElementById('root')
 )
